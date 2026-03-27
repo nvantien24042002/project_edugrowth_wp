f7c7d85ea5d01b513704231525a5046e40fe3065
Tien Nguyen <87626763+nvantien24042002@users.noreply.github.com>
2026-03-27 17:28:19 +0700

    update v5

--- wp-content/themes/tienThemes/page-ecosystem.php
+++ wp-content/themes/tienThemes/page-ecosystem.php
@@ -1,12 +1,28 @@
 <?php get_header()  ?>
 <div id="content">
+
+    <?php
+
+    $arg = array(
+        'post_title' => 'MARKET RESEARCH',
+        'post_desc' => 'Explore key trends in education innovation and EdTech from leaders across education, hand-picked global innovators and the Australia EdTech ecosystem.',
+        'category_name' => 'thought-leadership',
+        'orderby' => "DATE",
+        'order' => 'DESC',
+        'posts_per_page' => 9,
+        'category__not_in' => array(15, 13, 6, 12, 14, 5),
+    )
+
+    ?>
     <?php get_template_part('template-parts/banner') ?>
     <?php get_template_part('template-parts/ecosystem/advocates') ?>
     <?php get_template_part('template-parts/ecosystem/card') ?>
     <?php get_template_part('template-parts/ecosystem/strategic') ?>
     <?php get_template_part('template-parts/section/solution') ?>
 
-    <?php the_content() ?>
+
+
+    <?php get_template_part('template-parts/post', "", $arg) ?>
     <?php get_template_part('template-parts/section/membership') ?>
     <?php get_template_part('template-parts/events') ?>
 </div>
